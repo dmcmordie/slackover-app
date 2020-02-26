2b8805945a34d6de101bf97e90cee7fd51f6ef72
Harry Peach <harrypeach1999@gmail.com>
2020-02-26 14:55:57 +0000

    Hid keys

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "<your key here>",
-  authDomain: "<your key here>",
-  databaseURL: "<your key here>",
-  projectId: "<your key here>",
-  storageBucket: "<your key here>",
-  messagingSenderId: "<your key here>",
-  appId: "<your key here>8"
+  apiKey: "AIzaSyAOSai2kBa_K7aiH3ODuXipM-URvMIUsWs",
+    authDomain: "slackover-5e335.firebaseapp.com",
+    databaseURL: "https://slackover-5e335.firebaseio.com",
+    projectId: "slackover-5e335",
+    storageBucket: "slackover-5e335.appspot.com",
+    messagingSenderId: "175691899703",
+    appId: "1:175691899703:web:8dac4140dd42644c5070b2"
 };
 // Initialize Firebase
 firebase.initializeApp(config);
